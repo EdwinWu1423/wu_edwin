b926c35f47aa93dc7d48e762aedc98797b21107e
Edwin Wu <edwinwu@Edwins-MacBook-Pro.local>
2019-01-31 19:56:57 -0500

    Has convertible pic

--- Work0.py
+++ Work0.py
@@ -1,9 +1,10 @@
 f = open('image.ppm','w')
-f.write('P3 500 500 255')
+f.write('P3\n500 500\n255\n')
+
 for value in range (500):
     for secvalue in range (500):
-        r = 255
+        r = value / 2
         g = value % 256
         b = secvalue % 256
-        f.write(str(r)+ ' ' + str(g) + ' '+ str(b) + ' ')
+        f.write(str(r)+ ' ' + str(g) + ' ' +str(b)+ "\n")
 f.close()
